9446a478cfeb58f8d205c940ca71f2c7203ce6e5
Luigi Maglanoc <luigi.maglanoc@usit.uio.no>
2024-02-20 14:34:49 +0100

    substantial changes to python code

--- main.py
+++ main.py
@@ -1,60 +1,58 @@
 import os.path
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
+import requests
+import json
 import pandas as pd
-
-
-def set_chrome_options() -> Options:
-    """Sets chrome options for Selenium.
-    Chrome options for headless browser is enabled.
-    """
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_prefs = {}
-    chrome_options.experimental_options["prefs"] = chrome_prefs
-    chrome_prefs["profile.default_content_settings"] = {"images": 2}
-    return chrome_options
 
 
 class EmptyDataException(Exception):
     pass
 
+def parse_wesbite(url=None, payload=None):
+    response = requests.post(url, data=json.dumps(payload))
 
-def parse_webpage(soup):
+    # Assuming response.content contains the JSON string,
+    # convert the bytes to a string
+    json_string = response.content.decode()  
+
+    # Parse the JSON string
+    return json.loads(json_string)
+
+
+def parse_data(json_data):
     """ Get title and author details"""
 
-    all_books = soup.find_all("a", class_="itemNorli-name-2Vz")
-    all_authors = soup.find_all("div", class_="itemNorli-authorName-1ZR")
+    author_list = [hit["authors_name"] if isinstance(hit["authors_name"], list) 
+                   else [hit["authors_name"]] for hit in json_data["results"][0]["hits"]]
 
-    author_list = [author.get_text() for author in all_authors]
-    book_list = [book.get_text() for book in all_books]
+    book_list = [hit["name"] for hit in json_data["results"][0]["hits"]]
 
     if not author_list or not book_list:
         raise EmptyDataException(
             "Something wrong with book or author lists (likely no data)")
+    
+    book_details = zip(author_list, book_list)
 
-    return author_list, book_list
+    book_details = pd.DataFrame(book_details, columns=['Author names', 'Book'])
+
+    # remove square brackets from "Author names"
+    book_details['Author names'] = book_details['Author names'].apply(', '.join)
 
 
-def create_csv_file(author_list, book_list, old_file_exists, old_file, filepath):
+    return book_details
+
+
+
+def create_csv_file(book_details, old_file_exists, old_file, filepath):
     """Create csv-file from parsed webpage"""
-    data = {"author": author_list, "title": book_list,
-            "rank": list(range(1, len(book_list)+1))}
-
-    data_pd = pd.DataFrame.from_dict(data)
 
     if not old_file_exists:
-        data_pd.to_csv(filepath, index=False)
+        book_details.to_csv(filepath, index=False)
     else:
-        if not old_file.equals(data_pd):
-            data_pd.to_csv(filepath, index=False)
+        if not old_file.equals(book_details):
+            book_details.to_csv(filepath, index=False)
 
 
-def main():
+def main(url=None, payload=None):
     """ Run all """
     output_filepath = "output/topp-10-norli.csv"
 
@@ -65,21 +63,25 @@ def main():
         old_file_exists = False
         old_file = ""
 
-    driver = webdriver.Chrome(options=set_chrome_options())
+    norli_data = parse_wesbite(url=url, payload=payload)
 
-    url = "https://www.norli.no/boker/aktuelt-og-anbefalt/topplister/topp-10-boker"
-    driver.get(url)
-
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
-
-    author_list, book_list = parse_webpage(soup)
-
-    create_csv_file(author_list, book_list, old_file_exists,
+    book_details = parse_data(norli_data)
+    
+    create_csv_file(book_details, old_file_exists,
                     old_file, filepath=output_filepath)
 
-    driver.close()
 
 
 if __name__ == "__main__":
-    main()
+    url = "https://aeeyieersj-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia for JavaScript (4.19.1); Browser (lite)&x-algolia-api-key=ZjRlODkxZDFhY2MyOTE1YWU1OGRjZTRlYjZlMTViZDkwNWNmYjE3YmU2MjU4YzhlOGZjZjg4MTc3ZDY4M2FjYnJlc3RyaWN0U291cmNlcz0xOTMuMTU3LjIwNy4xODkmdGFnRmlsdGVycz0=&x-algolia-application-id=AEEYIEERSJ"
+
+    payload = {
+        "requests":
+            [
+                {
+                    "indexName":"norli_pwa_products","params":"clickAnalytics=true&facets=%5B%22format%22%2C%22authors_name%22%2C%22price.NOK.default%22%2C%22language%22%2C%22series%22%2C%22thema_subject%22%2C%22editionreleaseyear%22%2C%22color%22%2C%22number_of_pages%22%2C%22mammoth_number%22%2C%22categories.level0%22%5D&filters=categoryIds%3A5334&highlightPostTag=%3C%2Fais-highlight-0000000000%3E&highlightPreTag=%3Cais-highlight-0000000000%3E&hitsPerPage=48&maxValuesPerFacet=50&page=0&tagFilters="
+                }
+            ]
+        }
+    
+    main(url=url, payload=payload)
